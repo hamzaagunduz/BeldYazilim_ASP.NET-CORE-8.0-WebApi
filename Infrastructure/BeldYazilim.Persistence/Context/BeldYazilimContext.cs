022bf56f62e76ff7da127cbe2d87ad9794fbe9ba
Hamza Gündüz <81538263+hamzaagunduz@users.noreply.github.com>
2024-02-21 17:44:12 +0300

    Add project files.

---
+++ Infrastructure/BeldYazilim.Persistence/Context/BeldYazilimContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeldYazilim.Persistence.Context
+{
+    public class BeldYazilimContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-O82KITD;initial Catalog=BeldYazilimDb;integrated Security=true; TrustServerCertificate=True");
+
+        }
+    }
+}
