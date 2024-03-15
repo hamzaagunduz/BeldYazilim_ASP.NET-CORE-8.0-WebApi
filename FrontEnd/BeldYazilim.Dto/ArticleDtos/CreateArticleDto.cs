7c1fa713a8493dcf241fd2f94cda1cdaf4849a8d
Hamza Gündüz <81538263+hamzaagunduz@users.noreply.github.com>
2024-03-16 01:13:04 +0300

    Article Get+Post

---
+++ FrontEnd/BeldYazilim.Dto/ArticleDtos/CreateArticleDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeldYazilim.Dto.ArticleDtos
+{
+    public class CreateArticleDto
+    {
+            public string title { get; set; }
+            public string content { get; set; }
+            public DateTime creationTime { get; set; }
+            public int clickCount { get; set; }
+            public string bigImageUrl { get; set; }
+            public int rating { get; set; }
+
+
+    }
+}
--- FrontEnd/BeldYazilim.WebUI/Areas/Admin/Controllers/AdminArticleController.cs
+++ FrontEnd/BeldYazilim.WebUI/Areas/Admin/Controllers/AdminArticleController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BeldYazilim.Dto.ArticleDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace BeldYazilim.WebUI.Areas.Admin.Controllers
 {
@@ -6,9 +10,45 @@ namespace BeldYazilim.WebUI.Areas.Admin.Controllers
 
     public class AdminArticleController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminArticleController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7298/api/Article");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultArticleDtos>>(jsonData);
+                return View(values);
+            }
             return View();
         }
+        [HttpGet]
+        public async Task<IActionResult> CreateArticle()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateArticle(CreateArticleDto createArticleDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createArticleDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7298/api/Article", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(createArticleDto);
+        }
+
     }
 }
